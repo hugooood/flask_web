3998e536af308d190a0b397fe5fa2f10f5c453af
hugooood <hugooood@outlook.com>
2017-04-30 20:39:14 +0800

    .

--- Blog/app/__init__.py
+++ Blog/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object("config") ##从config.py读入配置
+
+app.config.from_object("config") #从config.py读入配置
 
 #这个import语句放在这里, 防止views, models import发生循环import
 from app import views,models
--- Blog/app/views.py
+++ Blog/app/views.py
@@ -4,3 +4,7 @@ from flask import render_template,url_for
 @app.route('/')
 def index():
 	return render_template('welcome.html',title="Welcome")
+
+@app.route('/home')
+def home():
+	return render_template('base.html',title="Home")
