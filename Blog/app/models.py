eec1c3e8381878a663eef0c548208191b1b8a504
hugooood <hugooood@outlook.com>
2017-05-04 22:00:13 +0800

    修改User模型 增加了email,password_hash,role_id字段

--- Blog/app/models.py
+++ Blog/app/models.py
@@ -3,12 +3,18 @@ from . import db
 from datetime import datetime
 import bleach,markdown #????
 
-# @loginManager.user_loader
-# def load_user(user_id):
-#     try:
-#         return User.query.get(int(user_id))
-#     except Exception as e:
-#         print(str(e))
+#密码散列
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+from . import login_manager
+#加载回调函数
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except Exception as e:
+        print(str(e))
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -25,10 +31,25 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    #设置密码
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    #验证密码
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return '<User %r>' % self.username
